5f985def56f2815dd8548020defcd4fbfb53f6b8
otchi012 <hiroshi012931@gmail.com>
2021-01-21 18:44:53 +0900

    add Chapter04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "asano";
+		System.out.println(name);
+
+		name = "hiroshi";
+		System.out.println(name);
+
+		final int lucky_num = 7;
+		System.out.println(lucky_num);
+
+	}
+}
