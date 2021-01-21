4ba5723c0556f441149e1b638e23cd08c67ef958
otchi012 <hiroshi012931@gmail.com>
2021-01-21 19:24:37 +0900

    add Chapter05

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 123L;
+		double double1 = 1.23d;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+
+		Float float2 = 3.21f;
+		Integer integer2 = 321;
+		Character character2 = 'A';
+		System.out.println(float2);
+		System.out.println(integer2);
+		System.out.println(character2);
+
+		int[] array = new int[] {10,20,30,40,50};
+		System.out.println(array[3]);
+	}
+}
