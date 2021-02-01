4221d3c83ed6062e32a03756a4348db0d0f3700a
otchi012 <hiroshi012931@gmail.com>
2021-02-01 19:30:55 +0900

    finish Chapter15_2

--- JavaWcp/src/practice/Chapter15_1.java
+++ JavaWcp/src/practice/Chapter15_1.java
@@ -3,7 +3,7 @@ package practice;
 public class Chapter15_1 {
 
 	public static void main(String[] args) {
-		int a = 0;
+		int a = 976;
 		int b = 427;
 
 		System.out.println(a + "と" + b + "の最大公約数は" + gcd(a, b) + "です。");
