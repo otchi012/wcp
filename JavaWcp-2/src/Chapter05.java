25aba7d36de2c5f47df83baf902d5fe3384198b1
otchi012 <hiroshi012931@gmail.com>
2021-02-04 16:25:59 +0900

    done Chapter05

---
+++ JavaWcp-2/src/Chapter05.java
@@ -0,0 +1,24 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		long long1 = 2124124L;
+		double double1 = 0.22d;
+		boolean boolean1 = true;
+
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+
+		Float float1 = 0.333f;
+		Integer int1 = 1234;
+		Character char1 = 'わ';
+
+		System.out.println(float1);
+		System.out.println(int1);
+		System.out.println(char1);
+
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.println(array[3]);
+	}
+}
