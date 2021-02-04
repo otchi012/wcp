b8fd37c703c7f98563be9b3977aed6800e47b506
otchi012 <hiroshi012931@gmail.com>
2021-02-04 16:09:07 +0900

    done Chapter04

---
+++ JavaWcp-2/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String name = "浅野";
+		System.out.println(name);
+
+		name = "稲川";
+		System.out.println(name);
+
+		//定数
+		final int num = 777;
+		System.out.println(num);
+	}
+}
