2b20fa5c68123bba21089e7187cf45004726fffb
otchi012 <hiroshi012931@gmail.com>
2021-02-10 19:58:32 +0900

    done Chapter10

--- JavaWcp-2/src/practice/Chapter10.java
+++ JavaWcp-2/src/practice/Chapter10.java
@@ -20,13 +20,26 @@ public class Chapter10 {
 		// ・throwSQLExceptionメソッドを呼び出し
 		// ・SQLExceptionをキャッチして、メッセージとスタックトレースを出力
 		// ・"throwSQLExceptionの呼び出し終了"のメッセージを出力
-
+		try {
+			Chapter10.throwSQLException();
+		} catch (SQLException e) {
+			System.out.println("SQLExceptionが発生。");
+			e.printStackTrace();
+		} finally {
+			System.out.println("throwSQLExceptionの呼び出し終了。");
+		}
 		System.out.println("mainメソッド終了");
 	}
 
 	// validIndexメソッドを作成
 	// ・indexがarrayのサイズの範囲内なら、インデックスの要素を出力
 	// ・サイズの範囲外なら、IllegalArgumentExceptionをスロー
+	public static void validIndex(int[] array, int index) {
+		if (array.length <= index) {
+			throw new IllegalArgumentException(index + "　はサイズの範囲外です");
+		}
+		System.out.println("インデックス" + index + "の要素は " + array[index] + "です。");
+	}
 
 	public static void throwSQLException() throws SQLException {
 		throw new SQLException("SQLエラーです");
