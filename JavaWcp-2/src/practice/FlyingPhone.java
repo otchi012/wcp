f428865e62abc92adb8cbe1ca5eaacc3ff2c77ea
otchi012 <hiroshi012931@gmail.com>
2021-02-15 16:52:47 +0900

    done chapter13

--- JavaWcp-2/src/practice/FlyingPhone.java
+++ JavaWcp-2/src/practice/FlyingPhone.java
@@ -4,4 +4,21 @@ import extend.phone.Phone;
 
 public class FlyingPhone implements Phone, Flying {
 	// プログラムを作成
+	private int mins;
+
+	public FlyingPhone(int mins) {
+		this.mins = mins;
+	}
+
+	public void call(String number) {
+		System.out.println(number + " に電話します。通話できるのは飛んでいる間だけです。");
+	}
+
+	public void fly() {
+		System.out.println(mins + " 分間、飛びます。 ");
+	}
+
+	public void powerOff() {
+		Flying.super.powerOff();
+	}
 }
